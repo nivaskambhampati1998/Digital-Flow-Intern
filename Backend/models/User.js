0813d4fac2713984bfc41169b4a251c59c3dc66d
Pavan Srinivas <nivaskambhampati1998@gmail.com>
2021-05-23 11:55:58 +0530

    Initial Commit

---
+++ Backend/models/User.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+let userSchema = new Schema({
+   username:{
+      type: String,
+      required: true
+   },
+   name:{
+       type: String,
+       require: true
+   },
+   email: {
+      type: String,
+      required: true
+   },
+   password: {
+      type: String,
+      required: true
+   },
+
+  /* password_confirmation : {
+      type: String,
+      required : true
+   },*/
+
+   address: {
+    type: String,
+    required: true
+ },
+   mobile: {
+    type: Number,
+    required: true
+ }
+},
+
+  {
+   timestamps: true,
+   collection: 'users'
+})
+module.exports = mongoose.model('User', userSchema);
